1e3637aeef621b7e2a3a7495d76d16e08e648f3e
Chelsea Voss <csvoss@mit.edu>
2015-05-13 00:26:28 -0400

    More tests

--- orgo/engine/test_reactions.py
+++ orgo/engine/test_reactions.py
@@ -17,5 +17,24 @@ class TestReactions(unittest.TestCase):
 
     def test1(self):
         self.assertReaction(hydrobrominate_it, "CC=C", "CC(Br)C")
+        self.assertReaction(hydrobrominate_it, "CC(C)=C", "CC(C)(Br)C")
+        self.assertReaction(hydrobrominate_it, "CCC", "CCC")
 
-    def test2
+    def test2(self):
+        self.assertReaction(hydrogenate_it, "CC=C", "CCC")
+        self.assertReaction(hydrogenate_it, "CC=C(C)C", "CCC(C)C")
+        self.assertReaction(hydrogenate_it, "CC#C", "CCC")
+        self.assertReaction(hydrogenate_it, "CC#CCC=C", "CCCCCC")
+        self.assertReaction(hydrogenate_it, "CC#CC#CCC=CCC=C", "CCCCCCCCCCC")
+        self.assertReaction(hydrogenate_it, "CCC", "CCC")
+        self.assertReaction(hydrogenate_it, "CCBr", "CCBr")
+
+    def test3(self):
+        self.assertReaction(hydroiodinate_it, "CC=C(CC)C", "CCC(I)(CC)C")
+        self.assertReaction(hydroiodinate_it, "C=C", "ICC")
+        self.assertReaction(hydroiodinate_it, "CCC", "CCC")
+
+    def test4(self):
+        self.assertReaction(hydrochlorinate_it, "C1CC=CCC1", "C1CCC(Cl)CC1")
+        self.assertReaction(hydrochlorinate_it, "C1CC(C)=CCC1", "C1CCC(Cl)(C)CC1")
+        self.assertReaction(hydrochlorinate_it, "CCC", "CCC")
